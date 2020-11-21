1902b8f7752fcd5c78d6ffc4b80d812e5a1e65ad
Kakashi_Hatake <pon4ickoff@yandex.ru>
2020-11-21 21:36:46 +0200

    Kolyun_228

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -17,7 +17,7 @@ public class SceneController : MonoBehaviour
             _enemy.transform.position = new Vector3(0, 0, 0);
             float angle = Random.Range(0, 360);
             _enemy.transform.Rotate(0, angle, 0);
+
         }
-        
     }
 }
